19178319d69bcef80c18afe04fc867efe9841354
qinjialei <qinjialei@foxmail.com>
2020-07-19 22:18:44 +0800

    Update

--- angular-project/projects/app-pc/src/app/app.module.ts
+++ angular-project/projects/app-pc/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
-
 @NgModule({
   declarations: [
     AppComponent
